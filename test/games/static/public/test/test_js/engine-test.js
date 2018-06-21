ba8cc07cb84be1b477d1eea77f07f7bcd5383947
quentin <quentin.huyghes@hotmail.com>
2018-06-21 09:51:10 +0200

    tests modifications

--- test/games/static/public/test/test_js/engine-test.js
+++ test/games/static/public/test/test_js/engine-test.js
@@ -4,12 +4,6 @@ QUnit.test( "clone", function( assert ) {
     assert.deepEqual( engine, e, "Cloned" );
 });
 
-QUnit.test( "get_possible_move_list", function( assert ) {
-    let engine = new window.OpenXum.Tintas.Engine();
-    let e = engine.get_possible_move_list();
-    assert.ok( e, "Passed!" );
-});
-
 QUnit.test( "is_finished", function( assert ) {
     let engine = new window.OpenXum.Tintas.Engine();
     assert.notOk( engine.is_finished(), "Not finished" );
@@ -22,6 +16,26 @@ QUnit.test( "change_player", function( assert ) {
     engine.change_player();
     assert.notEqual( engine._current, window.OpenXum.Tintas.Player.PLAYER_1, "Not player 1" );
     assert.deepEqual( engine._current, window.OpenXum.Tintas.Player.PLAYER_2, "Player 2" );
+});
+
+QUnit.test( "get_possible_move_list", function( assert ) {
+    let engine = new window.OpenXum.Tintas.Engine();
+    let engine2 = new window.OpenXum.Tintas.Engine();
+    engine.get_board()._id_pawn=0;
+    engine2.get_board()._id_pawn=8;
+    let tab=[];
+    let tab2=[];
+    let a=engine.get_possible_move_list();
+    let b=engine2.get_possible_move_list();
+    for(let i=0;i<a.length;i++){
+        tab.push(a[i].get_to());
+    }
+    for(let i=0;i<b.length;i++){
+        tab2.push(b[i].get_to());
+    }
+
+    assert.deepEqual( tab, [1,4,2], "Pass in id 0" );
+    assert.deepEqual( tab2, [4,11,16,12,5,2], "Pass in id 8" );
 });
 
 /* TO DO
@@ -40,7 +54,7 @@ QUnit.test( "add_points", function( assert ) {
     assert.deepEqual( engine._player_1[0], 1, "Added" );
 });
 
-QUnit.test( "add_points", function( assert ) {
+QUnit.test( "have_empty_stack", function( assert ) {
     let engine = new window.OpenXum.Tintas.Engine();
     let engine2 = new window.OpenXum.Tintas.Engine();
     engine._player_1[0]=1;
@@ -50,7 +64,6 @@ QUnit.test( "add_points", function( assert ) {
     engine._player_1[4]=1;
     engine._player_1[5]=1;
     engine._player_1[6]=1;
-
     engine2._player_1[0]=0;
     engine2._player_1[1]=0;
     engine2._player_1[2]=0;
@@ -58,13 +71,9 @@ QUnit.test( "add_points", function( assert ) {
     engine2._player_1[4]=0;
     engine2._player_1[5]=0;
     engine2._player_1[6]=0;
-    console.log(engine.have_empty_stack(window.OpenXum.Tintas.Player.PLAYER_1));
 
-    console.log(engine2.have_empty_stack(window.OpenXum.Tintas.Player.PLAYER_1));
-
-    assert.notOk( engine.have_empty_stack(window.OpenXum.Tintas.Player.PLAYER_1), "Not empty" );
-    assert.ok( engine2.have_empty_stack(window.OpenXum.Tintas.Player.PLAYER_1), "Added" );
-    assert.ok( 1===1, "Added" );
+    assert.notOk( engine.have_empty_stack(engine._player_1), "Not empty" );
+    assert.ok( engine2.have_empty_stack(engine2._player_2), "Added" );
 });
 
 QUnit.test( "player_stack_win", function( assert ) {
