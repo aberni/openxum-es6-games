2cb64cb6d265067c96a49207f4ea56c9d5eebd3b
quentin <quentin.huyghes@hotmail.com>
2018-06-21 15:54:20 +0200

    end of tests

--- test/games/static/public/test/test_js/coordinates-test.js
+++ test/games/static/public/test/test_js/coordinates-test.js
@@ -1,7 +1,8 @@
-// MARCHE PAS
 QUnit.test( "is_valid", function( assert ) {
     let coordinates = new window.OpenXum.Tintas.Coordinates(1);
-    assert.ok( coordinates.is_valid(), "Passed!" );
+    let coordinates2 = new window.OpenXum.Tintas.Coordinates(50);
+    assert.ok( coordinates.is_valid(), "Valid" );
+    assert.notOk( coordinates2.is_valid(), "Not valid" );
 });
 
 QUnit.test( "matrix", function( assert ) {
@@ -10,12 +11,12 @@ QUnit.test( "matrix", function( assert ) {
     coordinates2.matrix([1,1,1]);
     let coord = [coordinates.get_pos_x()+1,coordinates.get_pos_y()+1,coordinates.get_pos_z()+1];
     let coord2 = [coordinates2.get_pos_x(),coordinates2.get_pos_y(),coordinates2.get_pos_z()];
-    assert.deepEqual( coord, coord2, "Passed!" );
+    assert.deepEqual( coord, coord2, "Coordinates changed" );
 });
 
 QUnit.test( "clone", function( assert ) {
     let coordinates = new window.OpenXum.Tintas.Coordinates(1);
     let coord = coordinates.clone();
-    assert.deepEqual( coord, coordinates, "Passed!" );
+    assert.deepEqual( coord, coordinates, "Cloned" );
 });
 
