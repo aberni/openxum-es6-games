641d677a8a16798d6e4ae6b382a18576a342ec69
Antoine Bernier <bernier.antoine3@gmail.com>
2018-06-22 13:28:40 +0200

    fix IA / move.to_string()

--- test/games/static/neutreeko/test_ia_neutreeko.mjs
+++ test/games/static/neutreeko/test_ia_neutreeko.mjs
@@ -5,8 +5,8 @@ let white_win = 0;
 
 for (let i = 0; i < 5; ++i) {
     let e = new lib.OpenXum.Neutreeko.Engine(lib.OpenXum.Neutreeko.GameType.STANDARD, lib.OpenXum.Neutreeko.Color.BLACK);
-    let p1 = new lib.OpenXum.Neutreeko.IA.IAAlphaBeta(lib.OpenXum.Neutreeko.Color.BLACK, e);
-    let p2 = new lib.OpenXum.MCTSPlayer(lib.OpenXum.Neutreeko.Color.WHITE, e);
+    let p1 = new lib.OpenXum.MCTSPlayer(lib.OpenXum.Neutreeko.Color.BLACK, e);
+    let p2 = new lib.OpenXum.Neutreeko.IA.IAAlphaBeta(lib.OpenXum.Neutreeko.Color.WHITE, e);
     let p = p1;
 
     while (!e.is_finished()) {
@@ -14,7 +14,7 @@ for (let i = 0; i < 5; ++i) {
         p = p === p1 ? p2 : p1;
     }
 
-    console.log("Winner is " +(e.winner_is() === lib.OpenXum.Hnefatafl.Color.BLACK ? "black" : "white"));
+    console.log("Winner is " +(e.winner_is() === lib.OpenXum.Neutreeko.Color.BLACK ? "black" : "white"));
     if (e.winner_is() === lib.OpenXum.Neutreeko.Color.BLACK) {
         black_win++;
     } else {
